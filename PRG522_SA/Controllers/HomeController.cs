70b04f774d3f4d95ee41a18ffad61f66b8e173dd
DevonEco <devon.economon@gmail.com>
2022-10-31 11:35:06 +0200

    1

---
+++ PRG522_SA/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRG522_SA.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+    }
+}
